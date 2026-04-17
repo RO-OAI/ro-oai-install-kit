adad2b3a9b373750c1dd052372662724622ebb57
Alexandru Botici <alexandru.botici@external.wolt.com>
2026-04-17 19:39:55 +0300

    Fix shortcut script

---
+++ fix.ps1
@@ -0,0 +1,11 @@
+﻿$DesktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
+$OniaDest = Join-Path $DesktopPath "ONIA"
+
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut((Join-Path $OniaDest "Open_in_VSCode.lnk"))
+$Shortcut.TargetPath = $VSCodePath
+$Shortcut.Arguments = "`"$OniaDest`""
+$Shortcut.WorkingDirectory = $OniaDest
+$Shortcut.Description = "Open ONIA folder in VS Code"
+$Shortcut.Save()
+Write-Host "VS Code shortcut created in ONIA folder." -ForegroundColor Green
